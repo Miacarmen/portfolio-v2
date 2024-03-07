6437d01008ed1e8dfa16cc11368474ce168e5680
Miacarmen <maitreyacarmen@gmail.com>
2024-03-07 14:39:37 -0800

    pages laid out, daisyui installed

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -1,9 +1,51 @@
-/** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/**/*.{html,js}"],
+  content: ['./src/**/*.{js,jsx,ts,tsx}'],
+  daisyui: {
+    themes: [
+      {
+        mytheme: {
+          primary: '#362624',
+
+          secondary: '#FFCAE0',
+
+          accent: '#2A1F1E',
+
+          // 'accent-focus': '#FFB624',
+
+          neutral: '#f8f8ff', 
+
+          'base-100': '#1a1a22',
+
+          'base-200': '#1e1e1e',
+
+          'base-300': '#383838',
+
+          'base-content': '#050505',
+
+          info: '#3ABFF8',
+
+          success: '#78dcca',
+
+          warning: '#FBBD23',
+
+          error: '#F87272',
+        },
+      },
+    ],
+  },
   theme: {
     extend: {},
+    screens: {
+      sm: '480px',
+      md: '768px',
+      lg: '976px',
+      xl: '1440px',
+    },
+    colors: {},
   },
-  plugins: [],
-}
+  plugins: [require('daisyui')],
+};
+
+
+
 
