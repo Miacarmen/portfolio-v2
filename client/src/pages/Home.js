7f06aabec5c27df4787ed7aa30b9004fea603a5a
Miacarmen <maitreyacarmen@gmail.com>
2024-03-07 16:22:27 -0800

    main content added to home page

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -2,10 +2,26 @@ import React from 'react'
 
 const Home = () => {
   return (
-    <div>
-        <div className='container'>
+    <div className='main-container'>
+        {/* <div className='col-1'>
+                <h2>GH</h2>
+            </div> */}
+        <div className='grid-container'>
+            
+            <div className='col-1'>
             <h1 className='special'>MIA</h1>
             <h1 className='special'>CARMEN</h1>
+            <p className='tagline'>Full Stack Developer / UX/UI Designer</p>
+            </div>
+            <div className='col-2'>
+                <ul className='home-nav'>
+                    <li>
+                        WORK
+                    </li>
+                    <li>ABOUT</li>
+                    <li>CONTACT</li>
+                </ul>
+                </div>
             </div>
     </div>
   )
