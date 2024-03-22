7c73e94e58c46836337b787a30fc6b64d231ea53
Miacarmen <maitreyacarmen@gmail.com>
2024-03-21 19:22:53 -0700

    navbar conditionally rendered

--- client/src/components/Navigation/NavBar.js
+++ client/src/components/Navigation/NavBar.js
@@ -4,36 +4,70 @@ import { Link, useLocation } from 'react-router-dom';
 const NavBar = () => {
   const location = useLocation();
   const isHomePage = location.pathname === '/';
+  const isProjectPage = location.pathname.startsWith('/work/');
 
   return (
     <div className='nav-container'>
-      {isHomePage ? (
-        <div className='social-nav'>
-          <ul className='social-links'>
-            <Link
-              to='https://github.com/Miacarmen'
-              style={{ textDecoration: 'none' }}
-            >
-              <li>GH</li>
-            </Link>
-            <Link
-              to='https://www.linkedin.com/in/maitreya-carmen-7750a6b8/'
-              style={{ textDecoration: 'none' }}
-            >
-              <li>LI</li>
-            </Link>
+      {isHomePage && (
+        <div
+          className='social-nav'
+          style={{
+            position: 'fixed',
+            top: 0,
+            left: 0,
+            padding: '2rem',
+            transform: 'rotate(-90deg)',
+          }}
+        >
+          <ul
+            className='social-links'
+            style={{
+              listStyle: 'none',
+              display: 'flex',
+              gap: '1rem',
+              fontFamily: 'Lato, sans-serif',
+              fontWeight: 100,
+              fontSize: '20px',
+            }}
+          >
+            <li>
+              <a
+                href='https://github.com/Miacarmen'
+                target='_blank'
+                rel='noopener noreferrer'
+                style={{ color: '#f8f8ff', textDecoration: 'none' }}
+              >
+                GH
+              </a>
+            </li>
+            <li>
+              <a
+                href='https://www.linkedin.com/in/maitreya-carmen-7750a6b8/'
+                target='_blank'
+                rel='noopener noreferrer'
+                style={{ color: '#f8f8ff', textDecoration: 'none' }}
+              >
+                LI
+              </a>
+            </li>
           </ul>
         </div>
-      ) : (
-        <div className='page-specific-nav'>
-          {/* <Link to='/work' style={{ textDecoration: 'none' }}>
-            <p>WORK</p>
-          </Link> */}
+      )}
 
-          <Link to='/' className='home-link' style={{ textDecoration: 'none' }} >
-            <p>HOME</p>
+      {isProjectPage ? (
+        <div className='work-link'>
+          <Link to='/work' style={{ textDecoration: 'none' }}>
+            <p>WORK</p>
           </Link>
         </div>
+      ) : (
+        !isHomePage && (
+          <div className='home-link'>
+            <Link to='/' style={{ textDecoration: 'none' }}>
+              <p>HOME</p>
+            </Link>
+          </div>
+        )
       )}
     </div>
   );
